bd32aed9e784dfec2adceca2d5abf82d684ad801
Jean-Michel Feurprier <jmfeurprier@users.noreply.github.com>
2023-07-02 01:06:18 -0400

    Initial commit

---
+++ src/Bootstrap/Bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Bootstrap;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+readonly class Bootstrap
+{
+    /**
+     * @var InitializerInterface[]
+     */
+    private array $initializers;
+
+    public function __construct(InitializerInterface ...$initializers)
+    {
+        $this->initializers = $initializers;
+    }
+
+    public function boot(ContainerInterface $container): void
+    {
+        foreach ($this->initializers as $initializer) {
+            $initializer->initialize($container);
+        }
+    }
+}
