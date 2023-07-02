e5f53725e4f9c3d091e6797adcf1b7b2366a6ded
Jean-Michel Feurprier <jmfeurprier@gromweb.com>
2023-07-02 02:21:49 -0400

    wip

--- src/Bootstrap/Bootstrap.php
+++ src/Bootstrap/Bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Bootstrap;
+namespace Jmf\Bootstrap;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
--- src/Bootstrap/InitializerBase.php
+++ src/Bootstrap/InitializerBase.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Bootstrap;
+namespace Jmf\Bootstrap;
 
 use RuntimeException;
 use Symfony\Component\DependencyInjection\ContainerInterface;
